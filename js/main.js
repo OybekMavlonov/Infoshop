8ffc406d01dab58b36b615f75f452d2c399f9643
Oybek Mavlonov <oybek25108@gmail.com>
2021-03-19 10:05:14 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,143 @@
+// Slick Slider
+$(".slider").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // autoplay: true,
+  speed: 300,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 762,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 470,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+// CounterUp
+$(".number").counterUp({
+  delay: 10,
+  time: 2000,
+});
+$(".number").addClass("animated fadeInDownBig");
+
+//Mobile Menu
+(function ($) {
+  var size;
+
+  //SMALLER HEADER WHEN SCROLL PAGE
+  function smallerMenu() {
+    var sc = $(window).scrollTop();
+    if (sc > 40) {
+      $("#header-sroll").addClass("small");
+    } else {
+      $("#header-sroll").removeClass("small");
+    }
+  }
+
+  // VERIFY WINDOW SIZE
+  function windowSize() {
+    size = $(document).width();
+    if (size >= 1200) {
+      $("body").removeClass("open-menu");
+      $(".hamburger-menu .bar").removeClass("animate");
+    }
+  }
+
+  // ESC BUTTON ACTION
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+      $(".bar").removeClass("animate");
+      $("body").removeClass("open-menu");
+      $(
+        "header .desk-menu .menu-container .menu .menu-item-has-children a ul"
+      ).each(function (index) {
+        $(this).removeClass("open-sub");
+      });
+    }
+  });
+
+  $("#cd-primary-nav > li").hover(function () {
+    $whidt_item = $(this).width();
+    $whidt_item = $whidt_item - 8;
+
+    $prevEl = $(this).prev("li");
+    $preWidth = $(this).prev("li").width();
+    var pos = $(this).position();
+    pos = pos.left + 4;
+    $("header .desk-menu .menu-container .menu>li.line").css({
+      // width: $whidt_item,
+      // left: pos,
+      // opacity: 1,
+    });
+  });
+
+  // ANIMATE HAMBURGER MENU
+  $(".hamburger-menu").on("click", function () {
+    $(".hamburger-menu .bar").toggleClass("animate");
+    if ($("body").hasClass("open-menu")) {
+      $("body").removeClass("open-menu");
+    } else {
+      $("body").toggleClass("open-menu");
+    }
+  });
+
+  $("header .desk-menu .menu-container .menu .menu-item-has-children ul").each(
+    function (index) {
+      $(this).append('<li class="back"><a href="#">Back</a></li>');
+    }
+  );
+
+  // RESPONSIVE MENU NAVIGATION
+  $("header .desk-menu .menu-container .menu .menu-item-has-children > a").on(
+    "click",
+    function (e) {
+      e.preventDefault();
+      if (size <= 1200) {
+        $(this).next("ul").addClass("open-sub");
+      }
+    }
+  );
+
+  // CLICK FUNCTION BACK MENU RESPONSIVE
+  $(
+    "header .desk-menu .menu-container .menu .menu-item-has-children ul .back"
+  ).on("click", function (e) {
+    e.preventDefault();
+    $(this).parent("ul").removeClass("open-sub");
+  });
+
+  $("body .over-menu").on("click", function () {
+    $("body").removeClass("open-menu");
+    $(".bar").removeClass("animate");
+  });
+
+  $(document).ready(function () {
+    windowSize();
+  });
+
+  $(window).scroll(function () {
+    smallerMenu();
+  });
+
+  $(window).resize(function () {
+    windowSize();
+  });
+})(jQuery);
